36342e442c96f82c2db3829a1bb78b17d7464e23
techyaura <techyaura@gmail.com>
2021-02-28 01:50:04 +0530

    update folder structure

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 const PORT = 8000;
-app.get('/', (req,res) => res.send('Express + TypeScript Starter'));
+app.get('/', (req,res) => res.send('Express + TypeScript Starter !!'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
