8a7bcbbe5101cc0e58fcb262658713c79ab0b7fb
techyaura <techyaura@gmail.com>
2021-02-28 02:15:55 +0530

    add eslint comment

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ const app = express();
 const PORT = 8000;
 app.get('/', (req,res) => res.send('Express + TypeScript Starter !!'));
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
